00a0b0de260d3c58202bf36279fddd3b0c392fcd
Eyasu Kifle <eyasu.kifle@gmail.com>
2018-06-21 14:11:21 +0000

    Basic shell script for ensuring deps are included in package.json

---
+++ scripts/test-dependencies.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Make sure tests run with a clean node_modules. 
+# The purpose is to reduce the chance of forgetting to include dependencies in package.json
+
+
+cd ../
+
+# Stop PM2
+pm2 stop ecosystem.config.js
+
+# Test the client is sane
+cd client;
+rm -rf node_modules;
+rm -rf dist;
+npm install;
+npm test;
+
+cd ../
+
+# Test the server is sane
+cd server;
+rm -rf node_modules;
+npm install;
+npm test;
