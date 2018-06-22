bb98c75ae75cd90fe3ba798db5f1f6657718815c
Eyasu <eyasu.kifle@gmail.com>
2018-06-22 15:42:58 -0700

    Replace Vuetify with Bootstrap Vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,49 +1,7 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      fixed
-      app
-    >
-      <!--<v-list dense>-->
-      <!--  <v-list-tile @click="">-->
-      <!--    <v-list-tile-action>-->
-      <!--      <v-icon>home</v-icon>-->
-      <!--    </v-list-tile-action>-->
-      <!--    <v-list-tile-content>-->
-      <!--      <v-list-tile-title>Home</v-list-tile-title>-->
-      <!--    </v-list-tile-content>-->
-      <!--  </v-list-tile>-->
-      <!--  <v-list-tile @click="">-->
-      <!--    <v-list-tile-action>-->
-      <!--      <v-icon>contact_mail</v-icon>-->
-      <!--    </v-list-tile-action>-->
-      <!--    <v-list-tile-content>-->
-      <!--      <v-list-tile-title>Contact</v-list-tile-title>-->
-      <!--    </v-list-tile-content>-->
-      <!--  </v-list-tile>-->
-      <!--</v-list>-->
-    </v-navigation-drawer>
-    <v-toolbar color="indigo" dark fixed app>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Cruddy</v-toolbar-title>
-    </v-toolbar>
-    <v-content>
-      <v-container fluid fill-height>
-        <v-layout
-          justify-center
-          align-center
-        >
-          <v-flex text-xs-center>
-            <img src="./assets/logo.png">
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
-    <v-footer color="indigo" app>
-      <span class="white--text">&copy; 2018</span>
-    </v-footer>
-  </v-app>
+<div>
+App
+</div>
 </template>
 
 <script>
