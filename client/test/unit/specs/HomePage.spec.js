5963c38e80f320d399b0c6094c392ced9e2f4a66
Eyasu Kifle <eyasu.kifle@gmail.com>
2018-06-25 09:00:59 +0000

    Use Chrome Headless instead of PhantomJS, Fix test

--- client/test/unit/specs/HomePage.spec.js
+++ client/test/unit/specs/HomePage.spec.js
@@ -6,7 +6,7 @@ describe('Home.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(Home);
     const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Cruddy');
+    expect(vm.$el.querySelector('h1').textContent)
+      .to.equal('Cruddy');
   });
 });
