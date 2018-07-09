f5aaa97378141f7a58c5004d67bd384628fecb03
Eyasu Kifle <eyasu.kifle@gmail.com>
2018-07-09 09:01:47 +0000

    Conditionally show logged in/out state

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,13 +1,11 @@
 <template>
 <b-navbar toggleable="md" type="dark" variant="info">
   <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-
   <b-navbar-brand href="#">Cruddy</b-navbar-brand>
   <b-collapse is-nav id="nav_collapse">
 
-
     <!-- Right aligned nav items -->
-    <b-navbar-nav v-if="user" class="ml-auto">
+    <b-navbar-nav v-if="isLoggedIn && user" class="ml-auto">
       <b-nav-item-dropdown right>
         <!-- Using button-content slot -->
         <template slot="button-content">
@@ -17,8 +15,8 @@
         <b-dropdown-item @click="logout" href="#">Signout</b-dropdown-item>
       </b-nav-item-dropdown>
     </b-navbar-nav>
-    
-    <b-navbar-nav v-if="user">
+
+    <b-navbar-nav v-if="isLoggedIn">
       <b-nav-item to="/dashboard">Dashboard</b-nav-item>
       <b-nav-item to="/report/list">Reports</b-nav-item>
     </b-navbar-nav>
@@ -36,14 +34,15 @@ import { mapActions, mapGetters, mapMutations } from 'vuex';
 export default ({
   name: 'nav-bar',
   computed: {
-    ...mapGetters({ user: 'users/getCopy' }),
+    ...mapGetters({ isLoggedIn: 'getLoggedInState', user: 'getCurrentUser' }),
   },
   methods: {
     ...mapActions({ authLogout: 'auth/logout' }),
-    ...mapMutations({ clearUsers: 'users/clearAll' }),
+    ...mapMutations({ clearUsers: 'users/clearAll', unsetLogin: 'UNSET_LOGIN' }),
     logout() {
       this.authLogout();
       this.clearUsers();
+      this.unsetLogin();
     },
   },
 });
