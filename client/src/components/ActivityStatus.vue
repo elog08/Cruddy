43b24b36839bb74fcf061ee95e44220acea235b5
Eyasu Kifle <eyasu.kifle@gmail.com>
2019-04-07 16:00:54 -0700

    Status indicator

--- client/src/components/ActivityStatus.vue
+++ client/src/components/ActivityStatus.vue
@@ -1,16 +1,34 @@
 <template>
-        <b-alert id="activityStatus" class="fixed-bottom"
-            :variant="status.type"
-            show>{{status.message}}
+        <b-alert v-if="status" 
+          id="activityStatus" 
+          v-on:click="toggle"
+          :class="'fixed-bottom' + this.ghost ? ' ghost' : ''"
+          :variant="status.type"
+          show>{{status.message}}
         </b-alert>
 </template>
 <style>
   #activityStatus {
     width: 130px;
   }
+
+  #activityStatus .ghost {
+    opacity: 0.4
+  }
 </style>
 <script>
 export default {
+  methods: {
+    toggle() {
+      console.info('Toggling');
+      this.ghost = !this.ghost;
+    }
+  },
+  data() {
+    return {
+      ghost: false
+    };
+  },
   computed: {
     status() {
       return this.$store.getters.getActivityStatus;
--- client/src/components/ConnectionStatus.vue
+++ client/src/components/ConnectionStatus.vue
@@ -1,16 +1,36 @@
 <template>
-        <b-alert id="connectionStatus" class="fixed-bottom"
+        <b-alert id="connectionStatus"
+            @click="toggle"
+            :class="this.ghost ? 'fixed-bottom ghost' : 'fixed-bottom'"
             :variant="status.type"
             show>{{status.message}}
         </b-alert>
 </template>
 <style>
   #connectionStatus {
+    user-select: none;
+    -webkit-user-select : none;
+    -moz-user-select : none;
+    -ms-user-select: none;
     width: 130px;
+  }
+  #connectionStatus.ghost {
+    opacity: 0.4
   }
 </style>
 <script>
 export default {
+  methods: {
+    toggle() {
+      console.info('Toggling');
+      this.ghost = !this.ghost;
+    }
+  },
+  data() {
+    return {
+      ghost: false
+    };
+  },
   computed: {
     status() {
       return this.$store.getters.getConnectionStatus;
